547ea043bb19d1a2d346ef7de099e08967939dd6
TDTxLE <tdtxle@gmail.com>
2024-03-01 20:31:29 -0600

    r

--- app/components/calculator.js
+++ app/components/calculator.js
@@ -6,30 +6,45 @@ export default class CalculatorComponent extends Component {
   @tracked model = { numero1: 0.0, numero2: 0.0 };
 
   sumar = () => {
+    let resultafo =
+      parseFloat(this.model.numero1) + parseFloat(this.model.numero2);
+
     this.resultados = [
       ...this.resultados,
-      parseFloat(this.model.numero1) + parseFloat(this.model.numero2),
+      `${this.model.numero1} + ${this.model.numero2} = ${resultafo}`,
     ];
   };
 
-  restar = () => {
+  _restar = () => {
+    let resultafo = parseFloat(this.model.numero1) - parseFloat(this.model.numero2);
+
     this.resultados = [
       ...this.resultados,
-      parseFloat(this.model.numero1) - parseFloat(this.model.numero2),
+      `${this.model.numero1} - ${this.model.numero2} = ${resultafo}`,
     ];
   };
+  get restar() {
+    return this._restar;
+  }
+  set restar(value) {
+    this._restar = value;
+  }
 
   multiplicar = () => {
+    let resultafo = parseFloat(this.model.numero1) * parseFloat(this.model.numero2);
+    
     this.resultados = [
       ...this.resultados,
-      parseFloat(this.model.numero1) * parseFloat(this.model.numero2),
+      `${this.model.numero1} * ${this.model.numero2} = ${resultafo}`,
     ];
   };
 
   dividir = () => {
+    let resultafo = parseFloat(this.model.numero1) / parseFloat(this.model.numero2);
+    
     this.resultados = [
       ...this.resultados,
-      parseFloat(this.model.numero1) / parseFloat(this.model.numero2),
+      `${this.model.numero1} / ${this.model.numero2} = ${resultafo}`,
     ];
   };
 }
